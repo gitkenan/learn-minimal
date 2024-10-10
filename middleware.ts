7559cb2a5a54258271e576bc9cd48033c70c178f
gitkenan <kenan.mth@gmail.com>
2024-10-10 08:32:29 +0100

    getting rid of clerk error at least

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,14 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware(() => {
+  return NextResponse.next();
+});
 
+// Temporarily match all routes for testing purposes
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-};
+    matcher: [
+      '/((?!api|_next/static|_next/image|favicon.ico|images|fonts).*)',
+    ],
+  };
+  
