6f207c55b4760cf3d99c7b9af2f09105007be2e7
gitkenan <kenan.mth@gmail.com>
2024-12-05 17:34:41 +0000

    fixing catchall

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,11 @@ import { NextResponse } from "next/server";
 export default clerkMiddleware({});
  
 export const config = {
+<<<<<<< HEAD
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
 
+=======
+  matcher: ["/", "/(api|trpc)(.*)", "/((?!.*\\..*|_next).*)"],
+};
+>>>>>>> f1f78ab (fixing catchall)
