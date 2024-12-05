6681435df850aa1dd65f00a22395fe0659b8311e
gitkenan <kenan.mth@gmail.com>
2024-12-05 17:28:42 +0000

    better fix for #5

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
-
+export default clerkMiddleware({});
+ 
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|sign-in|sign-up|about|contact).*)',
-    '/api/:path*'
-  ]
-}
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
