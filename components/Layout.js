319960b5da3b05c47d0b3a120799864634648e20
gitkenan <kenan.mth@gmail.com>
2025-02-02 09:26:18 +0100

    fix Layout bad effect on chat page

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 w-full relative">
+      <main className="flex-1 w-full relative flex flex-col">
         {children}
       </main>
     </div>
